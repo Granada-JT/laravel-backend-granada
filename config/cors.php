95ebeeca18b9239ca7c1f5e4f094434ca93c84ab
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-13 21:10:22 +0800

    feat: add fetch all users

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'login', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'login', 'sanctum/csrf-cookie', 'users'],
 
     'allowed_methods' => ['*'],
 
